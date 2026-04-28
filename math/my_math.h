8b7519387dc4934189e4553f6078e60a049d52d5
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-28 23:51:36 +0300

    Create my_math.h

---
+++ math/my_math.h
@@ -0,0 +1,10 @@
+#ifndef MY_MATH_H
+#define MY_MATH_H
+int my_abs(int x);
+int my_max(int a, int b);
+int my_min(int a, int b);
+int my_pow(int base, int exp);
+int my_fact(int n);
+int my_gcd(int a, int b);
+int my_lcm(int a, int b);
+#endif
