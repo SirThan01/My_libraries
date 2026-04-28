c2ff2823cd8fa08bc31a659705ea90a10abd0390
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-28 23:42:18 +0300

    Create print.h

---
+++ print/print.h
@@ -0,0 +1,9 @@
+#ifndef PRINT_H
+#define PRINT_H
+void print(const char* s);
+void println(const char* s);
+void print_int(int n);
+void print_float(float f, int decimals);
+void print_repeat(char c, int count);
+void print_center(const char* s, int width);
+#endif
