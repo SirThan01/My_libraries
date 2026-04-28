58450371da5d7a779e8de258b0912242f3003e5f
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-28 23:32:33 +0300

    Create color.h

---
+++ color/color.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_H
+#define COLOR_H
+void red(const char* s);
+void green(const char* s);
+void yellow(const char* s);
+void blue(const char* s);
+void magenta(const char* s);
+void cyan(const char* s);
+void reset(void);
+#endif
